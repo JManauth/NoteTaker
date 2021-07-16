f10665c922973de84ba10b3ac77cf517c9fda2cd
jafet <jafet.manauth@hotmail.com>
2021-07-16 16:18:49 -0500

    restructured folders and made api routes work

--- public/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-require('./routes/htmlroutes')(app);
-
-app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`);
-});
